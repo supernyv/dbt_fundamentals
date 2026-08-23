d189f442192ab56cdb36d28083bd23612161685e
Nyv Mondele Mbola <supernyv@gmail.com>
2026-08-23 23:38:18 +0100

    Update models

--- models/marts/marketing/dim_customer.sql
+++ models/marts/marketing/dim_customer.sql
@@ -10,6 +10,17 @@ from
 group by 1
 ),
 
+customer_spending AS (
+select
+    customer_order.customer_id,
+    sum(payment.amount) as lifetime_spending
+from
+    {{ref("stg_stripe__payment")}} as payment
+    left join {{ref("stg_jaffle_shop__customer_order")}} as customer_order
+    using (order_id)
+group by 1
+),
+
 
 final as (
 select
@@ -18,10 +29,13 @@ select
     customer.last_name,
     customer_order_summary.first_order_date,
     customer_order_summary.most_recent_order_date,
-    coalesce(customer_order_summary.number_of_orders, 0) as number_of_orders
+    coalesce(customer_order_summary.number_of_orders, 0) as number_of_orders,
+    coalesce(customer_spending.lifetime_spending, 0) as lifetime_spending
 from 
     {{ref("stg_jaffle_shop__customer")}} as customer
     left join customer_order_summary
+    using (customer_id)
+    left join customer_spending
     using (customer_id)
 
 )
--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -3,7 +3,7 @@ select
     order_id,
     payment_method,
     status,
-    amount,
-    created
+    amount/100 as amount, -- convert cents to dollars
+    created as created_at
 from
     raw.stripe.payment
