ad9bdba1e65d5109953d2ef8a48faa86a4effa71
Nyv Mondele Mbola <supernyv@gmail.com>
2026-08-24 17:38:32 +0100

    Update fact order model

--- models/marts/finance/fct_order.sql
+++ models/marts/finance/fct_order.sql
@@ -1,8 +1,9 @@
 select
     customer_order.customer_id,
     customer_order.order_id,
-    payment.amount
+    sum(case when status = 'success' then payment.amount end) as amount
 from
     {{ref("stg_jaffle_shop__customer_order")}} as customer_order
     inner join {{ref("stg_stripe__payment")}} as payment
     using (order_id)
+group by 1, 2
